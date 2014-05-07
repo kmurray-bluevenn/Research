fb0e9acbeb2f6281ce5e8a6f25e218a1b24171a2
kmurray-bluevenn <kmurray@bluevenn.com>
2014-05-07 12:12:38 +0100

    build log file added

---
+++ write build log.ps1
@@ -0,0 +1,8 @@
+﻿$now = [datetime]::Now.ToString()
+$logFile = [System.IO.Path]::Combine($Env:HOMEDRIVE + "\", "build log.txt")
+$logText = [string]::Format("CI Build started | {0}", $now)
+if (!(Test-Path -Path $logFile))
+{
+	New-Item -ItemType file -Path $logFile
+}
+Add-Content -Path $logFile -Value $logText
